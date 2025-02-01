2dbc55c422f9005cc8619e956c6a4f2c930a97e2
MINEDUCYT <MINEDUCYT@SWEETY>
2025-01-31 21:50:19 -0600

    Agregar archivos de proyecto.

---
+++ TexBox.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TexBox.aspx.cs" Inherits="TexBox_20135831.TexBox" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat ="server" Font-Bold = "true" Text = "Product Description"> </asp:Label>
+                    <br />
+            <asp:TextBox runat = "server" Columns = "50" Rows = "5" TextMode = "MultiLine" Wrap = "true"></asp:TextBox>
+                    <br />
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ TexBox.aspx.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TexBox_20135831
+{
+
+
+    public partial class TexBox
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
+}
